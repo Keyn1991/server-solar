ac13d78e468467284c44eedd77a4ed50b5c8d0ae
potap <potapchuk50@gmail.com>
2023-03-28 20:51:00 +0100

    git-2

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ app.get('/', (req, res) => {
     res.send('<h1>Server started on port 5100</h1>');
 });
 
-const PORT = process.env.PORT ||   10000;
+const PORT = process.env.PORT ||   5100;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
